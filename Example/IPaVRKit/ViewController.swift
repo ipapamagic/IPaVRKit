81c04946e551b08b0478ff72d0bad612555f8f16
IPa Chen <ipapamagic@gmail.com>
2021-12-10 19:02:50 +0800

    Initial commit

---
+++ Example/IPaVRKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  IPaVRKit
+//
+//  Created by IPa Chen on 12/10/2021.
+//  Copyright (c) 2021 IPa Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
